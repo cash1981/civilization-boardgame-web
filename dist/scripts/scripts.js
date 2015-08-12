b114592ff52a8c87c30c0c9d6c30e195fbdba6f4
Shervin Asgari <shervin@asgari.no>
2015-08-12 12:42:59 +0200

    Added undo on discard and removed revealing of culture and huts

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1400,7 +1400,7 @@ var GameController = function ($log, $routeParams, GameService, PlayerService, c
   };
 
   function checkPermissionForVote(log) {
-    return $scope.userHasAccess && log && log.draw && log.log.indexOf("drew") > -1;
+    return $scope.userHasAccess && log && log.draw && (log.log.indexOf("drew") > -1 || log.log.indexOf("discarded") > -1);
   }
 
   //In scope so that we can use it from another view which is included
