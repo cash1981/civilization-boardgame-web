3207e227c0c220665fd2719fb2d86af36d81d2c6
Shervin Asgari <shervin@asgari.no>
2015-10-05 19:56:36 +0200

    Adding boostrap and simplechat manually

--- grunt.sh
+++ grunt.sh
@@ -2,3 +2,10 @@
 
 grunt
 cp web.js dist/
+cp bower_components/angular-bootstrap-simple-chat/src/scripts/index.js dist/scripts/irontec-simplechat.js
+mkdir -p dist/styles/simplechat
+cp bower_components/angular-bootstrap-simple-chat/src/css/style.css dist/styles/simplechat/
+cp bower_components/angular-bootstrap-simple-chat/src/css/themes.css dist/styles/simplechat/
+cp bower_components/bootstrap/dist/css/bootstrap.min.css dist/styles/
+
+exit 0;
