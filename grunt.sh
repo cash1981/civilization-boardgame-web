660e13540b2ccfc46d39b0a83a054bc64f09534b
Shervin Asgari <shervin@asgari.no>
2015-10-05 20:17:28 +0200

    Trying to fix chat

--- grunt.sh
+++ grunt.sh
@@ -2,10 +2,9 @@
 
 grunt
 cp web.js dist/
-cp bower_components/angular-bootstrap-simple-chat/src/scripts/index.js dist/scripts/irontec-simplechat.js
+cp app/scripts/irontec-simplechat.js dist/scripts/irontec-simplechat.js
 mkdir -p dist/styles/simplechat
-cp bower_components/angular-bootstrap-simple-chat/src/css/style.css dist/styles/simplechat/
-cp bower_components/angular-bootstrap-simple-chat/src/css/themes.css dist/styles/simplechat/
+cp app/styles/simplechat/*.css dist/styles/simplechat/
 cp bower_components/bootstrap/dist/css/bootstrap.min.css dist/styles/bootstrap.css
 
 exit 0;
