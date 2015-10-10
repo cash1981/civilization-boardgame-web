0fd99a7225c8f12ab6401d59b38c612cbfbb62df
Shervin Asgari <shervin@asgari.no>
2015-10-10 12:03:08 +0200

    WIP

--- app/scripts/controllers/TurnsController.js
+++ app/scripts/controllers/TurnsController.js
@@ -3,9 +3,9 @@
   var TurnsController = function ($log, GameService, DrawService, currentUser, Util, growl, $routeParams, $scope) {
     var viewModel = this;
 
-
     var turns = [{
       turnnr: 1,
+      disabled: true,
       sot: "Scout to city F2, Create city @ A5",
       trade: 14,
       cm: "Harvest silk",
@@ -14,6 +14,7 @@
     },
       {
         turnnr: 2,
+        disabled: false,
         sot: "Nothing",
         trade: 23,
         cm: "Devote capital",
@@ -23,6 +24,13 @@
     ];
 
     viewModel.turns = turns;
+
+    viewModel.disableForm = function (turnIndex) {
+      console.log('disabled set to true');
+      var turn = viewModel.turns[turnIndex];
+      console.log("turn is ", turn);
+      turn.disabled = true;
+    }
   };
 
   module.controller("TurnsController",
